7fde03bfdc2f64226f97b9fa41c8d636e4984230
hooeni <hooeni@gmail.com>
2023-01-10 00:11:48 +0900

    H2 데이터베이스 설치 순수 JDBC 코딩 스프링 통합 테스트 스프링 JdbcTemplate 사용(더 공부 해야 할 부분)

--- src/main/java/hellospring/hello/SpringConfig.java
+++ src/main/java/hellospring/hello/SpringConfig.java
@@ -1,13 +1,24 @@
 package hellospring.hello;
 
 import hellospring.hello.repository.MemberRepository;
-import hellospring.hello.repository.MemoryMemberRepository;
+import hellospring.hello.repository.jdbcTemplateMemberRepository;
 import hellospring.hello.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+    private final DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
     @Bean
     public MemberService memberService(){
         return new MemberService(memberRepository());
@@ -15,6 +26,8 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository(){
-        return new MemoryMemberRepository();
+//        return new MemoryMemberRepository();
+//        return new JdbcMemberRepository(dataSource);
+        return new jdbcTemplateMemberRepository(dataSource);
     }
 }
